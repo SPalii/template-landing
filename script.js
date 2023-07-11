b33ed185f89eeb74af15f115193ed455a05c2a9a
SPalii <110282169+SPalii@users.noreply.github.com>
2023-07-11 16:52:18 +0300

    changed style nav

--- script.js
+++ script.js
@@ -12,16 +12,41 @@ function closeFullImg(){
 
 // button top
 let btnScrollToTop = document.querySelector("#btnScrollToTop");
-console.log(btnScrollToTop);
-
-btnScrollToTop.addEventListener("click", function () {
- window.scrollTo({
-   top: 0,
-   left: 0,
-   behavior: "smooth"
+  btnScrollToTop.addEventListener("click", function () {
+     window.scrollTo({
+     top: 0,
+     left: 0,
+     behavior: "smooth"
    });
-
 });
 
+
+// change bg nav
+
+function changeBg(){
+  var navbar = document.getElementById("navbar");
+  var scrollValue = window.scrollY;
+
+if(scrollValue > 350){
+  navbar.classList.add ("navbar-scrolled");
+}else{
+  navbar.classList.remove("navbar-scrolled");
+}}
+window.addEventListener("scroll", changeBg);
+
+
+// chahge nav text color --???
+
+function chahgeNavText(){
+    var navbar = document.getElementById('navbar');
+    var scrollValue = window.scrollY;
+    // console.log(scrollValue);
+
+  if (scrollValue >= innerHeight){
+     navbar.classList.add('navtext-scrolled');
+  }else{
+  navbar.classList.remove('navtext-scrolled');
+}}
+document.addEventListener('scroll', chahgeNavText);
 
 
